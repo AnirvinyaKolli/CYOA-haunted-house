73401975b534a6b8e670a213b6bb46ae7c8ba59f
AnirvinyaKolli <anirvinya.kolli.01@gmail.com>
2025-10-06 09:17:37 -0400

    format tetxt file

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
+#include <iostream> 
+#include <string> 
+#include <fstream> 
+using namespace std;
+
+    
+string formatTextFile(string url){
+    ifstream inputFile(url); 
+    string output = ""; 
+    string line; 
+    while (getline(inputFile, line)) {
+        output += (line + '\n'); 
+    }
+    return output;
+}
+
+
 int main()
 {
-    
     return 0;
 }
